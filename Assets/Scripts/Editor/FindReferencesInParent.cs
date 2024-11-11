1dc23c7a387028006df90af169b9fa0c010ad8fd
xiaobai00x <3311736006@qq.com>
2024-11-11 23:45:00 +0800

    start

---
+++ Assets/Scripts/Editor/FindReferencesInParent.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class FindReferencesInParent : MonoBehaviour
+{
+    private static HashSet<string> whiteList = new HashSet<string>()
+    {
+        "Transform",
+        "RectTransform",
+        "UIWidget",
+        "UIPanel",
+        "UIScrollView",
+        "UITable",
+        "UIGrid",
+        "UISprite",
+        "RecyclePrefabPool"
+    };
+
+    [MenuItem("GameObject/Find Reference In Parent", false, 0)]
+    static void FindReferenceInParent()
+    {
+        if (Selection.gameObjects.Length != 1)
+        {
+            Debug.LogWarning("<color=yellow>Please select one game object to find references in parent.</color>");
+            return;
+        }
+
+        GameObject selectedObj = Selection.gameObjects[0];
+        Component[] selectedComponents = selectedObj.GetComponents<Component>();
+        Transform parent = selectedObj.transform.parent;
+        while (parent != null)
+        {
+            Component[] components = parent.gameObject.GetComponents<Component>();
+            foreach (Component component in components)
+            {
+                if (component == null) continue;
+                if (whiteList.Contains(component.GetType().Name)) continue;
+                SerializedObject so = new SerializedObject(component);
+                SerializedProperty sp = so.GetIterator();
+                while (sp.NextVisible(true))
+                {
+                    if (sp.propertyType != SerializedPropertyType.ObjectReference) continue;
+                    if (sp.objectReferenceValue == null) continue;
+                    if (sp.objectReferenceValue == selectedObj)
+                    {
+                        string message = string.Format("Reference found in <color=yellow>{0}</color> for <color=yellow>{1}</color> referencing <color=yellow>{2}</color> on <color=yellow>{3}</color> at field <color=yellow>{4}</color>",
+                        parent.gameObject.name,
+                        component.GetType().Name,
+                        sp.objectReferenceValue.GetType().Name,
+                        selectedObj.name,
+                        sp.displayName);
+
+                        Debug.LogError(message);
+                        Selection.activeGameObject = parent.gameObject;
+                        EditorGUIUtility.PingObject(component);
+                        return;
+                        Debug.LogError("Reference found in <color=yellow>" + parent.gameObject.name + "</color> for <color=yellow>" + component.GetType().Name + "</color> referencing <color=yellow>" + selectedObj.name +"     -->"+ sp.displayName + "</color>");
+                        Selection.activeGameObject = parent.gameObject;
+                        EditorGUIUtility.PingObject(component);
+                        // EditorGUIUtility.ShowObjectPicker<Object>(component, false, "",
+                        //     EditorGUIUtility.GetControlID(FocusType.Passive));
+                        return;
+                    }
+
+                    foreach (Component selectedComponent in selectedComponents)
+                    {
+                        if (sp.objectReferenceValue == selectedComponent)
+                        {
+                            string message = string.Format("Reference found in <color=yellow>{0}</color> for <color=yellow>{1}</color> referencing <color=yellow>{2}</color> on <color=yellow>{3}</color> at field <color=yellow>{4}</color>",
+                            parent.gameObject.name,
+                            component.GetType().Name,
+                            sp.objectReferenceValue.GetType().Name,
+                            selectedObj.name,
+                            sp.displayName);
+
+                            Debug.LogError(message);
+                            Selection.activeGameObject = parent.gameObject;
+                            EditorGUIUtility.PingObject(component);
+                            return;
+                            Debug.LogError("Reference found in <color=yellow>" + parent.gameObject.name + "</color> for <color=yellow>" + component.GetType().Name + "</color> referencing <color=yellow>" + selectedComponent.GetType().Name + "</color> on <color=yellow>" + selectedObj.name +"     -->"+ sp.displayName + "</color>");
+                            Selection.activeGameObject = parent.gameObject;
+                            EditorGUIUtility.PingObject(component);
+                            // EditorGUIUtility.ShowObjectPicker<Object>(component, false, "",
+                            //     EditorGUIUtility.GetControlID(FocusType.Passive));
+                            return;
+                        }
+                    }
+                }
+            }
+
+            parent = parent.parent;
+        }
+
+        Debug.Log("No reference to <color=yellow>" + selectedObj.name + "</color> found in its parent objects.");
+    }
+}
---
+++ Assets/Scripts/Editor/FindReferencesWindow.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class FindReferencesWindow : EditorWindow
+{
+    private GameObject selectedObject;
+    private List<ReferenceItem> references = new List<ReferenceItem>();
+    private Vector2 scrollPosition;
+
+    [MenuItem("GameObject/Find References In Scene", false, 0)]
+    static void FindReferences()
+    {
+        // Open the window
+        var window = GetWindow<FindReferencesWindow>("References");
+        window.selectedObject = Selection.activeGameObject;
+        window.FindReferencesInScene();
+    }
+
+    void OnGUI()
+    {
+        if (selectedObject == null)
+        {
+            EditorGUILayout.HelpBox("No object selected.", MessageType.Info);
+            return;
+        }
+
+        EditorGUILayout.LabelField("Finding references for: ", selectedObject.name);
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+
+        GUIStyle highlightStyle = new GUIStyle(GUI.skin.button);
+        highlightStyle.richText = true;
+        foreach (var reference in references)
+        {
+            if (GUILayout.Button(reference.disPlayName + "-----" + reference.disPlayomponentName + "---->" + 
+                                 reference.componentName + " in " + reference.gameObjectName, highlightStyle))
+            {
+                if (reference.component == null)
+                {
+                    continue;
+                }
+                // Ping the object in the hierarchy to highlight it
+                EditorGUIUtility.PingObject(reference.component.gameObject);
+
+                // Select the object to show it in the Inspector
+                Selection.activeObject = reference.component;
+
+                // Focus the Inspector window
+                EditorWindow inspectorWindow = EditorWindow.GetWindow(typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow"));
+                if (inspectorWindow != null)
+                {
+                    inspectorWindow.Focus();
+                }
+            }
+        }
+
+        EditorGUILayout.EndScrollView();
+    }
+
+    void FindReferencesInScene()
+    {
+        references.Clear();
+        references.Add(new ReferenceItem
+        {
+            disPlayName = "在引用脚本中的名字",
+            disPlayomponentName = "引用类型",
+            componentName = "引用脚本",
+            gameObjectName = "引用节点",
+        });
+        var allComponents = FindObjectsOfType<Component>();
+        foreach (var component in allComponents)
+        {
+            if (component == null) continue;
+
+            SerializedObject so = new SerializedObject(component);
+            SerializedProperty sp = so.GetIterator();
+
+            while (sp.NextVisible(true))
+            {
+                if (sp.propertyType != SerializedPropertyType.ObjectReference) continue;
+                if (sp.objectReferenceValue == null) continue;
+
+                if (sp.objectReferenceValue == selectedObject || sp.objectReferenceValue is Component comp && comp.gameObject == selectedObject)
+                {
+                    references.Add(new ReferenceItem
+                    {
+                        disPlayName = "<color=yellow>" + sp.displayName + "</color>",
+                        disPlayomponentName = "<color=yellow>" + sp.objectReferenceValue.GetType().Name + "</color>",
+                        componentName = "<color=yellow>" + component.GetType().Name + "</color>",
+                        gameObjectName = component.gameObject.name,
+                        component = component
+                    });
+                }
+            }
+        }
+
+        Repaint();
+    }
+
+    struct ReferenceItem
+    {
+        public string componentName;
+        public string disPlayName;
+        public string disPlayomponentName;
+        public string gameObjectName;
+        public Component component;
+    }
+}
